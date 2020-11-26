6a325e0093da2b26068baf3752340190e4e56814
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-11-26 20:54:14 +0700

    [ADD] tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      listStyleType: {
+        square: 'square'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.mix.js
+++
@@ -1,4 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.sass('static/sass/app.scss', 'static/css/app.module.css')
-    .setPublicPath('static');
